b42610bcb1bc20878010a8f046ddc407a34545bd
unknown <ramonsiqueira80@gmail.com>
2023-06-13 13:18:24 -0300

    Iniciando projeto blogpessoal com react e Typescript

---
+++ BlogPessoalReact/src/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import"./home.css";
+
+function Home() {
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://i.imgur.com/sAs8Gfp.png" alt=" Farol" className="img"/>
+        
+        </>
+    
+    );
+    
+} 
+
+export default Home;
