39a5ad413dbb70db136c1eb74d3568663a336cae
unknown <ramonsiqueira80@gmail.com>
2023-06-16 13:49:50 -0300

    Colocando rotas

--- BlogPessoalReact/src/App.tsx
+++ BlogPessoalReact/src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import { Grid } from "@material-ui/core";
 import Home from './pages/home';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
-function App() {
- 
+function App(): JSX.Element {
+
 
   return (
-    <>
+    <BrowserRouter> 
+    {/* Tudo que precisa ser rederizado na tela precisa ficar dentro daqui */}
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '80vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
--- BlogPessoalReact/src/components/estaticos/footer/Footer.tsx
+++ BlogPessoalReact/src/components/estaticos/footer/Footer.tsx
@@ -3,40 +3,42 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
+import "./Footer.css"
+
 function Footer () {
 return (
     <>
-      <Grid container direction="row" justifyContent="center" alignItems="center">
+            <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className="box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
                             <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.facebook.com/ramon.siqueira80/" target="_blank" rel="noopener noreferrer">
+                                <FacebookIcon className="redes" />
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.instagram.com/ramon_as80/" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon className="redes" />
                             </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.linkedin.com/in/ramonsiqueira80/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon className="redes" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className="box2">
                         <Box paddingTop={1}>
                             <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2023 Copyright:</Typography>
                         </Box>
                         <Box>
-                            <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Criado por Ramon</Typography>
+                            <a target="_blank" href="https://github.com/Ramon-Siqueira" rel="noopener noreferrer">
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Ramon Siqueira</Typography>
                             </a>
                         </Box>
                     </Box>
                 </Grid>
             </Grid>
-    </>
+        </>
 )
 
 }
